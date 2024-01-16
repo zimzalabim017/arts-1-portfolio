b66416412a1d159222e6a9abfc4d176217186822
zimzalabim017 <156817028+zimzalabim017@users.noreply.github.com>
2024-01-16 22:31:38 +0800

    Added project files

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000, // Adjust as needed
+        arrows: true,
+        dots: true,
+    });
+});
